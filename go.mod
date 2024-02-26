fde35117694508b89e69bc070fa725e250ad1d25
Rizky Ziaul Haq <rizkyziaulhaq21@gmail.com>
2024-02-26 21:42:23 +0700

    create modul hoi

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rizkyziaulhaq/say-hoi
+
+go 1.21.6
---
+++ say_hoi.go
@@ -0,0 +1,18 @@
+package sayhoi
+
+func SayHai(name string) string {
+	return "Hai Zia" + name
+}
+
+// func CreateNewFile(name string, message string) error {
+// 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	defer file.Close()
+// 	file.WriteString(message)
+
+// 	return nil
+// }
