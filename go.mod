628dacfd89667a08fa37d3f8eda6f3b69e6ae59b
Rizky Ziaul Haq <rizkyziaulhaq21@gmail.com>
2024-02-26 22:17:42 +0700

    update say age

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rizkyziaulhaq/say-hoi
+module github.com/rizkyziaulhaq/say-hoi/v2
 
 go 1.21.6
--- say_hoi.go
+++ say_hoi.go
@@ -6,6 +6,10 @@ func SayHai(name string) string {
 	return "Hai Zia" + name
 }
 
+func SayAge() string {
+	return "Kamu keren"
+}
+
 func CreateNewFile(name string, message string) error {
 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
 
