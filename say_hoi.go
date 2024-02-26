9f65de6aa28c086e6e328a694d941538d51670b1
Rizky Ziaul Haq <rizkyziaulhaq21@gmail.com>
2024-02-26 21:58:48 +0700

    update create txt

--- say_hoi.go
+++ say_hoi.go
@@ -1,18 +1,20 @@
 package sayhoi
 
+import "os"
+
 func SayHai(name string) string {
 	return "Hai Zia" + name
 }
 
-// func CreateNewFile(name string, message string) error {
-// 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
+func CreateNewFile(name string, message string) error {
+	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
 
-// 	if err != nil {
-// 		return err
-// 	}
+	if err != nil {
+		return err
+	}
 
-// 	defer file.Close()
-// 	file.WriteString(message)
+	defer file.Close()
+	file.WriteString(message)
 
-// 	return nil
-// }
+	return nil
+}
